0e0fbcf1772a665fb3565894ce611ef537302f99
S. Meisha Ray <bluedragoness@fullsail.edu>
2011-09-14 13:51:56 -0400

    Added in my Math section to my code.

--- js/project3.js
+++ js/project3.js
@@ -5,7 +5,8 @@
 
 //variables
 
-var place = "The Chocolate Exchange",
+var form = true,
+	place = "The Chocolate Exchange",
 	time = 10,
 	money2 = 80,
 	cabRide = 75,
@@ -16,34 +17,37 @@ var place = "The Chocolate Exchange",
 	type2: "Russian Blueberry",
 	type3: "Expresso Rage",
 	type4: "Fudge Blast",
-	
-	form = false,	
+	type5: "Orange Passion",		
 };
-//Start//
-	console.log("Today I was looking through one of my kitchen cabinets and found a variety of different types of chocolates. I remember seeing a commercial on television about a place called " + place + ", and thought what a great idea. With all of the different types of chocolates I have, I could go and exchange them for some chocolates I would actually eat. I called Shanna and told her about my idea and she wanted to go with me.");
+var totalAmount = 80
+	taxiCabRide = 75/3	
 
+//Start//
+	console.log("Today I was looking through one of my kitchen cabinets and found a variety of different types of chocolates. I remember seeing a commercial on television about a place called " + place + ", and thought what a great idea. With all of the different types of chocolates I have, I could go and exchange them for some chocolates I would actually eat. I called Shanna and Crystal and told them about my idea. They both wanted to go with me to " + place + ".");
+
+	console.log(Math.floor(Math.random()*11));
 //Arrival at The Chocolate Exchange//
-	var getTaxiCabRide = function(){
-		var taxiCabRide = 75/2
-			console.log("How much do we owe you?");
+		var getTaxiCabRide = function(){
+		var taxiCabRide = 75/3			
+		console.log("How much do we owe you?");
 			console.log("$75.00 please."); 
-			console.log("We each owe $" + taxiCabRide + "0 to the cab driver.");
+			console.log("We each owe $" + taxiCabRide + " to the cab driver.");
 			console.log("Here you go, $80.00");
 			console.log("Thank you. Here is your change.");
 	
-	//while-loop//	
+//while-loop//	
 		while(money2 > cabRide){
 		console.log(money2 - cabRide);
 		money2--;
 		}
 		};
 			getTaxiCabRide();
-	
+		
 	console.log("Awesome, let\'s go inside.");
 			
 //Inside The Chocolate Exchange//
 	console.log("Hello. My name is " + counterLady + ". How may I help you?");
-	if(time < 10){
+	if(time < 12){
 	console.log("Hello. Good Morning.");
 	}else{
 		console.log("Hello. Good Afternoon.");
@@ -73,14 +77,35 @@ var place = "The Chocolate Exchange",
 	console.log("I\'d also like to purchase some chocolates as well.");
 	console.log("Feel free to shop and you can exchange these when you check out.");
 	console.log("I want to buy some other types of chocolate.");
+	console.log("I want to get ");
+	console.log(Math.floor(Math.random()*8));
+	console.log(otherTypes.type2);
+	console.log("and ");
+	console.log(Math.floor(Math.random()*19));
+	console.log(otherTypes.type3);	
 	
 	
 //Answering questions on form//
+//boolean//
 	console.log("Can you three please feel out these customer inquiry forms?");
-		if(form = true){
-	console.log("Yes we can.")
+			if(form = true){
+		console.log("Yes we can.")
 		}else{
-	console.log("I'm sorry, we don't have time. Maybe next time.");
-	};		
+			console.log("I'm sorry, we don't have time. Maybe next time.");
+	};	
+
+	console.log("Here you go. Just fill in the info there.");
+		var handleData3 = function(json){
+			for(var f=0; f < json.myForm.length; f++){
+				var info = json.myForm[f];
+					console.log(info.name + ", " + info.age + ", " + info.phone);
+					};
+				};
+					handleData3(json3);	
+	console.log("Thank you very much Ladies. Come back and see us. Have a great day.");
+	
+//Left the store//
+	console.log("Taxi!");
+	console.log("THE END");
 		
 	
